7181874771306e3863fdb2f205eb4853beb9cda0
Andrew <cosminfuica@gmail.com>
2022-11-03 18:55:12 +0200

    mike-breach

--- Assignments/Assignment 1/GitAssignment/ConsoleUI/Program.cs
+++ Assignments/Assignment 1/GitAssignment/ConsoleUI/Program.cs
@@ -6,6 +6,6 @@ Console.WriteLine("Hello, World!");
 Console.Write("What is your name: ");
 string name = Console.ReadLine();
 
-Console.WriteLine($"Hello {name}");
+Console.WriteLine($"Hello {name}, have a great day");
 
 Console.ReadLine();
