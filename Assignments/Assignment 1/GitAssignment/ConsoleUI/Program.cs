2aa97e6776a6d35b40017f044c6f251285ebc0b5
Andrew <cosminfuica@gmail.com>
2022-11-03 17:08:15 +0200

    Asking user input and displaying a message

--- Assignments/Assignment 1/GitAssignment/ConsoleUI/Program.cs
+++ Assignments/Assignment 1/GitAssignment/ConsoleUI/Program.cs
@@ -1,4 +1,11 @@
-﻿
+﻿using System;
+
 
 Console.WriteLine("Hello, World!");
+
+Console.Write("What is your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}");
+
 Console.ReadLine();
