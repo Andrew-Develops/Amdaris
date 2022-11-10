946d37c459e42b226736fb62619074374e2ca949
Andrew <cosminfuica@gmail.com>
2022-11-10 21:55:21 +0200

    assignment 6

---
+++ Assignments/Assignment 6/GenericDemo/GenericDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+GenericCollection<string> genericCollection = new GenericCollection<string>(3);
+genericCollection.Add("Hello");
+genericCollection.Add("World");
+genericCollection.Add("!");
+
+// Swap two items
+genericCollection.Swap(0, 1);
+
+// Print out the items in the list
+for (int i = 0; i < genericCollection.Size(); i++)
+{
+    Console.WriteLine("item: "+genericCollection.GetItem(i));
+}
+
+Console.ReadLine();
+
+
+
+// Create a generic collection class using array
+public class GenericCollection<T> 
+{
+    private T[] array;
+    private int nextIndex = 0;
+
+    public GenericCollection(int size)
+    {
+        array = new T[size];
+    }
+
+    public void Add(T item)
+    {
+        array[nextIndex] = item;
+        nextIndex++;
+    }
+
+    // Get item at given index
+    public T GetItem(int index)
+    {
+        return array[index];
+    }
+
+    // Set item at given index method
+    public void SetItem(int index, T item)
+    {
+        array[index] = item;
+    }
+    
+    // Swap two items method
+    public void Swap(int index1, int index2)
+    {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+    // get size of array
+    public int Size()
+    {
+        return array.Length;
+    }
+}
---
+++ Assignments/Assignment 6/README.md
@@ -0,0 +1,6 @@
+# Assignment
+
+* Create a generic Collection class (using array []) having following operations
+* Get item at given index
+* Set item at given index
+* Swap two items (either by index or by items or both)
