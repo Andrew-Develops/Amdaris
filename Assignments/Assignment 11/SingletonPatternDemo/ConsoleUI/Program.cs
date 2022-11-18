3ca14f6f1f0f38735cd83e2020d246bc62f52114
Andrew <cosminfuica@gmail.com>
2022-11-18 14:50:22 +0200

    Singleton pattern redone

---
+++ Assignments/Assignment 11/SingletonPatternDemo/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleUI;
+
+public class Program
+{
+    private static TableServers host1List = TableServers.GetTableServersInstance();
+    private static TableServers host2List = TableServers.GetTableServersInstance();
+
+    private static void Main(string[] args)
+    {
+        TableServers servers = TableServers.GetTableServersInstance();
+
+        for (int i = 0; i < 10; i++)
+        {
+            //Console.WriteLine($"The next server is: {servers.GetNextServer()}");
+            Host1GetNextServer();
+            Host2GetNextServer();
+        }
+
+        Console.WriteLine();
+    }
+
+    public static void Host1GetNextServer()
+    {
+        Console.WriteLine($"The next server is: {host1List.GetNextServer()}");
+    }
+
+    public static void Host2GetNextServer()
+    {
+        Console.WriteLine($"The next server is: {host2List.GetNextServer()}");
+    }
+}
---
+++ Assignments/Assignment 11/SingletonPatternDemo/ConsoleUI/TableServers.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleUI
+{
+    public class TableServers
+    {
+        // Singura modalitate de a instantia clasa este :"new TableServers()"
+        private static readonly TableServers _instance = new TableServers();
+
+        private List<string> _servers = new List<string>();
+        private int _nextServer = 0;
+
+        private TableServers()
+        {
+            _servers.Add("Mike");
+            _servers.Add("John");
+            _servers.Add("Bob");
+            _servers.Add("Marry");
+        }
+
+        public static TableServers GetTableServersInstance()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer()
+        {
+            string output = _servers[_nextServer];
+            _nextServer++;
+
+            if (_nextServer >= _servers.Count)
+            {
+                _nextServer = 0;
+            }
+
+            return output;
+        }
+    }
+}
